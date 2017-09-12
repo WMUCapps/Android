654a3aaaecc9741e5347afd6ed7d033db07d7a8f
Evan Richard <Evan@Evans-MacBook-Pro-2.local>
2017-09-11 22:22:34 -0400

    Path name change

---
+++ app/src/main/java/android_radio/ListItem.java
@@ -0,0 +1,41 @@
+package android_radio;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Evan on 4/16/17.
+ */
+
+public class ListItem extends View {
+    private String time, show, host;
+
+    public ListItem(Context context, String time, String show, String host) {
+        super(context);
+        this.time = time;
+        this.show = show;
+        this.host = host;
+    }
+
+    public String getTime() { return time; }
+
+    public String getShow() {
+        return show;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setShow(String show) {
+        this.show = show;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
